cd761f78694962745c0978ac90c14b51a48515de
Dmitry <northoskol@gmail.com>
2023-09-08 15:09:15 +0300

    создание репозитория

---
+++ Lection/lection1/main.py
@@ -0,0 +1,17 @@
+# print("введите первое число:")
+
+# a=input()
+# b=input('введите второе число:')
+# print(a, '+', b, '=', a+b)
+
+
+"""c=5.89
+print(c)
+print(type(c))
+c= str(c)
+print(type(c))"""
+
+"""a = 5.9595595
+b = 4.621984
+print(round(a*b, 3))"""
+
