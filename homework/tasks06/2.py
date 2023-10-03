4b86f4d36c7b6930e91dfaa41015bb3915a9731e
Dmitry <northoskol@gmail.com>
2023-10-03 13:02:46 +0300

    add hw06

---
+++ homework/tasks06/2.py
@@ -0,0 +1,5 @@
+a1 = 2
+d = 3
+n = 4
+for i in range(n):
+    print(a1 + i * d)
--- seminars/seminar006/task43.py
+++ seminars/seminar006/task43.py
@@ -16,9 +16,33 @@
 
 """генератор чисел"""
 
-from random import randint
+# from random import randint
+#
+# list_1 = [randint(1, 10) for _ in range(10)]
+# print(list_1)
+# pair_count = sum(list_1.count(el) // 2 for el in set(list_1))
+# print(pair_count)
 
-list_1 = [randint(1, 10) for _ in range(10)]
-print(list_1)
-pair_count = sum(list_1.count(el) // 2 for el in set(list_1))
-print(pair_count)
+import random
+
+
+def count_elements(list_1):
+    count = 0
+    for i in range(1, len(list_1) - 1):
+        if list_1[i - 1] < list_1[i] and list_1[i] > list_1[i + 1]:
+            count += 1
+    return count
+
+
+def great_fill_list(numbers, start, finish):
+    new_list = [random.randint(start, finish) for i in range(numbers)]
+    return new_list
+
+
+n = int(input("Введите количество элементов массива: "))
+start_l = int(input("Введите начало диапазона элементов массива: "))
+finish_l = int(input("Введите начало конец элементов массива: "))
+my_list = great_fill_list(n, start_l, finish_l)
+print("Сгенерирован массив: ")
+print(my_list)
+print(f"Количество элементов у которых оба соседних меньше = {count_elements(my_list)}")
